85fa6d65e84522c4ce4636be57d6c9672b0ca877
Halil Yaman <40357552+halilyaman@users.noreply.github.com>
2019-10-19 00:41:22 +0300

    node types were created

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,35 +1,128 @@
+
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+enum NodeTypes {
+   StartNode,
+   EndNode,
+   AvailableNode,
+   WallNode,
+   PathNode,
+   SearchedNode,
+}
 
 public class MainWindow {
+   final private int ROW_SIZE = 20;
+   final private int COL_SIZE = 50;
+
    private JFrame frame = new JFrame();
    private JPanel panel = new JPanel();
    private JPanel gridViewPanel = new JPanel();
+   private Node[][] nodes = new Node[ROW_SIZE][COL_SIZE];
+
+   //index0 start node, index1 end node
+   private Node[] previousNodes = new Node[2];
 
    void buildScreen() {
       panel.setLayout(new BorderLayout());
-      gridViewPanel.setLayout(new GridLayout(20, 50));
+      gridViewPanel.setLayout(new GridLayout(ROW_SIZE, COL_SIZE));
       gridViewPanel.setBorder(new EmptyBorder(80,50,80,50));
       buildGridView();
       panel.add(gridViewPanel);
-      frame.add(panel);
-      frame.setSize(1700, 850);
-      frame.setVisible(true);
+
+      frame.setLayout(null);
+      frame.setContentPane(panel);
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setLocation(0,0);
+      frame.setSize(new Dimension(1500, 800));
+      frame.setVisible(true);
+      frame.setResizable(true);
    }
 
    private void buildGridView() {
-      for(int row = 0; row < 20; row++) {
-         for(int col = 0; col < 50; col++) {
-            if(row == 10 && col == 10) {
-               gridViewPanel.add(new Node(row, col, Color.green).buildNode());
-            } else if(row == 15 && col == 40) {
-               gridViewPanel.add(new Node(row, col, Color.red).buildNode());
-            } else {
-               gridViewPanel.add(new Node(row, col, Color.white).buildNode());
-            }
+      for(int row = 0; row < ROW_SIZE; row++) {
+         for(int col = 0; col < COL_SIZE; col++) {
+            nodes[row][col] = new Node(row, col, NodeTypes.AvailableNode);
+            nodes[row][col].getNodePanel().addMouseListener(new NodeMouseListener(nodes[row][col]));
+            gridViewPanel.add(nodes[row][col].getNode());
          }
+      }
+   }
+
+   private void updateNode(int val, Node selectedNode) {
+      if(val == 1) {
+         if(previousNodes[0] == null) {
+            previousNodes[0] = selectedNode;
+            selectedNode.setNodeType(NodeTypes.StartNode);
+         } else {
+            if(selectedNode.getNodeType() == NodeTypes.EndNode) {
+               previousNodes[1] = null;
+            }
+            previousNodes[0].setNodeType(NodeTypes.AvailableNode);
+            previousNodes[0] = selectedNode;
+            selectedNode.setNodeType(NodeTypes.StartNode);
+         }
+      } else if(val == 0) {
+         if(previousNodes[1] == null) {
+            previousNodes[1] = selectedNode;
+            selectedNode.setNodeType(NodeTypes.EndNode);
+         } else {
+            if(selectedNode.getNodeType() == NodeTypes.StartNode) {
+               previousNodes[0] = null;
+            }
+            previousNodes[1].setNodeType(NodeTypes.AvailableNode);
+            previousNodes[1] = selectedNode;
+            selectedNode.setNodeType(NodeTypes.EndNode);
+         }
+      }
+   }
+
+   class NodeMouseListener implements MouseListener {
+
+      private Node selectedNode;
+      private String[] optionTextList = {"end", "start"};
+
+      NodeMouseListener(Node selectedNode) {
+         this.selectedNode = selectedNode;
+      }
+
+      @Override
+      public void mouseClicked(MouseEvent e) {
+         int val;
+         val = JOptionPane.showOptionDialog(
+            selectedNode.getNodePanel(),
+            "Choose the Type of the Node",
+            "",
+            JOptionPane.DEFAULT_OPTION,
+            JOptionPane.INFORMATION_MESSAGE,
+            null,
+            optionTextList,
+            optionTextList[1]
+         );
+         updateNode(val, selectedNode);
+      }
+
+      @Override
+      public void mousePressed(MouseEvent e) {
+
+      }
+
+      @Override
+      public void mouseReleased(MouseEvent e) {
+
+      }
+
+      @Override
+      public void mouseEntered(MouseEvent e) {
+
+      }
+
+      @Override
+      public void mouseExited(MouseEvent e) {
+
       }
    }
 }
--- src/Node.java
+++ src/Node.java
@@ -5,17 +5,90 @@ import java.awt.*;
 public class Node {
    private JPanel nodePanel = new JPanel();
    private JPanel inlineNodePanel = new JPanel();
+
+   private NodeTypes nodeType;
    private int row;
    private int col;
    private Color color;
 
-   Node(int row, int col, Color color) {
-      this.row = row;
-      this.col = col;
-      this.color = color;
+   public void setNodePanel(JPanel nodePanel) {
+      this.nodePanel = nodePanel;
    }
 
-   JPanel buildNode() {
+   public JPanel getInlineNodePanel() {
+      return inlineNodePanel;
+   }
+
+   public void setInlineNodePanel(JPanel inlineNodePanel) {
+      this.inlineNodePanel = inlineNodePanel;
+   }
+
+   public NodeTypes getNodeType() {
+      return nodeType;
+   }
+
+   public void setNodeType(NodeTypes nodeType) {
+      this.nodeType = nodeType;
+      if(nodeType == NodeTypes.AvailableNode) {
+         this.setColor(Color.white);
+      } else if(nodeType == NodeTypes.StartNode) {
+         this.setColor(Color.green);
+      } else if(nodeType == NodeTypes.EndNode) {
+         this.setColor(Color.red);
+      } else if(nodeType == NodeTypes.WallNode) {
+         this.setColor(Color.black);
+      } else if(nodeType == NodeTypes.PathNode) {
+         this.setColor(Color.yellow);
+      } else if(nodeType == NodeTypes.SearchedNode) {
+         this.setColor(Color.orange);
+      }
+   }
+
+   public int getRow() {
+      return row;
+   }
+
+   public void setRow(int row) {
+      this.row = row;
+   }
+
+   public int getCol() {
+      return col;
+   }
+
+   public void setCol(int col) {
+      this.col = col;
+   }
+
+   public Color getColor() {
+      return color;
+   }
+
+   public void setColor(Color color) {
+      this.color = color;
+      inlineNodePanel.setBackground(this.color);
+   }
+
+   public JPanel getNodePanel() {
+      return nodePanel;
+   }
+
+   Node(int row, int col, NodeTypes nodeType) {
+      this.row = row;
+      this.col = col;
+      this.nodeType = nodeType;
+      if(nodeType == NodeTypes.AvailableNode) {
+         this.color = Color.white;
+      } else if (nodeType == NodeTypes.StartNode) {
+         this.color = Color.green;
+      } else if(nodeType == NodeTypes.EndNode) {
+         this.color = Color.red;
+      } else if(nodeType == NodeTypes.WallNode) {
+         this.color = Color.black;
+      }
+   }
+
+   JPanel getNode() {
       nodePanel.setBorder(new EmptyBorder(0,0,3,3));
       nodePanel.setLayout(new BorderLayout());
       inlineNodePanel.setBorder(BorderFactory.createLineBorder(Color.black, 2));
